72b070085f5a36718a9032dd759b4744c389149a
dogaatac <dogaatac1905@gmail.com>
2025-04-06 16:58:00 +0300

    1x

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ PLOT_CANDLES_AFTER = 5
 PLOT_TIMEFRAME = "15m"  # Mum zaman dilimi
 
 # Genel ayarlar
-SYMBOLS = ["BTCUSDT","ETHUSDT","BNBUSDT"]  # İşlem çiftleri
+SYMBOLS = ["BTCUSDT","ETHUSDT","BNBUSDT","SOLUSDT","DOGEUSDT"]  # İşlem çiftleri
 DATA_WINDOW = 250  # Kaç mum geriye bakılacak
-PROXIMITY_THRESHOLD = 0.001  # Pivot yakınlık eşiği (%0.1)
+PROXIMITY_THRESHOLD = 0.002  # Pivot yakınlık eşiği (%0.1)
